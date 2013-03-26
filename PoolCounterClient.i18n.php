7df78adf367f59d26526db7da302da3fb98b0f84
shirayuki <shirayuking@gmail.com>
2013-03-26 19:21:11 +0900

    Add full stops to error messages
    Change-Id: Ia5bec0bcf9e230462dad1555bd1a3d90f0d44181

--- PoolCounterClient.i18n.php
+++ PoolCounterClient.i18n.php
@@ -14,8 +14,8 @@ $messages = array();
 $messages['en'] = array(
 	'poolcounter-desc' => 'MediaWiki client for the pool counter daemon',
 	'poolcounter-connection-error' => 'Error connecting to pool counter server: $1',
-	'poolcounter-read-error' => 'Error reading from pool counter server',
-	'poolcounter-write-error' => 'Error writing to pool counter server',
+	'poolcounter-read-error' => 'Error reading from pool counter server.',
+	'poolcounter-write-error' => 'Error writing to pool counter server.',
 	'poolcounter-remote-error' => 'Pool counter server error: $1',
 );
 
