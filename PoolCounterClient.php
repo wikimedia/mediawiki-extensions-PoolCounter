77febde8a677c55fa0678bef4dcf71c1603e809f
ananay <i@ananayarora.com>
2015-12-13 15:08:39 +0530

    Add extension.json, empty PHP entry point
    Bug:T87960
    Change-Id: I27d7837bab3f4aedf7f0481df78e5d1e0a95d16c

--- PoolCounterClient.php
+++ PoolCounterClient.php
@@ -4,44 +4,9 @@
  * MediaWiki client for the pool counter daemon poolcounter.py.
  */
 
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Pool Counter Client',
-	'author'         => 'Tim Starling',
-	'descriptionmsg' => 'poolcounter-desc',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:PoolCounter',
-);
-
-
-/**
- * Configuration array for the connection manager.
- * Use $wgPoolCounterConf to configure the pools.
- */
-$wgPoolCountClientConf = array(
-	/**
-	 * Array of hostnames, or hostname:port. The default port is 7531.
-	 */
-	'servers' => array( '127.0.0.1' ),
-
-	/**
-	 * Connect timeout
-	 */
-	'timeout' => 0.1,
-);
-
-/**
- * Sample pool configuration:
- *   $wgPoolCounterConf = array( 'ArticleView' => array(
- *     'class' => 'PoolCounter_Client',
- *     'timeout' => 15, // wait timeout in seconds
- *     'workers' => 5, // maximum number of active threads in each pool
- *     'maxqueue' => 50, // maximum number of total threads in each pool
- *   ) );
- */
-
-$dir = __DIR__ . '/';
-$wgAutoloadClasses['PoolCounter_ConnectionManager']
-	= $wgAutoloadClasses['PoolCounter_Client']
-	= $dir . 'PoolCounterClient_body.php';
-$wgMessagesDirs['PoolCounterClient'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['PoolCounterClient'] = $dir . 'PoolCounterClient.i18n.php';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'PoolCounter' );
+	$wgMessagesDirs['PoolCounterClient'] = __DIR__ . '/i18n';
+} else {
+	die( 'This version of the Pool Counter extension requires MediaWiki 1.25+' );
+}
