f5acd57e442617a6a3180a5e107e0151bd2ce51e
Reedy <reedy@wikimedia.org>
2016-07-11 22:22:48 +0100

    De-dupe old PHP entry points
    Change-Id: I1b403888124a262a8e124ffee1ed2f2a7055470f

--- PoolCounterClient.php
+++ PoolCounterClient.php
@@ -1,12 +1,3 @@
 <?php
 
-/**
- * MediaWiki client for the pool counter daemon poolcounter.py.
- */
-
-if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'PoolCounter' );
-	$wgMessagesDirs['PoolCounterClient'] = __DIR__ . '/i18n';
-} else {
-	die( 'This version of the Pool Counter extension requires MediaWiki 1.25+' );
-}
+require_once( 'PoolCounter.php' );
